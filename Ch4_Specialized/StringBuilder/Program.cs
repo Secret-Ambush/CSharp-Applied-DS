7268040a3be49a5fe227b6bbec112b74cce97c4e
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-04 11:14:53 +0400

    Update Program.cs

--- Ch4_Specialized/StringBuilder/Program.cs
+++ Ch4_Specialized/StringBuilder/Program.cs
@@ -1,4 +1,4 @@
-﻿// Example file for C# Applied Data Structures by Joe Marini
+// Example file for C# Applied Data Structures by Joe Marini
 // Using StringBuilder
 
 using System;
@@ -10,27 +10,35 @@ public sealed class App
     {
         // TODO: Create a StringBuilder that expects to hold 50 characters.
         // Initialize the StringBuilder with "ABC".
+        StringBuilder sb = new StringBuilder(50);
+        Console.WriteLine($"Capacity: {sb.Capacity}, MaxCapacity: {sb.MaxCapacity}");
 
 
         // TODO: Append three characters (D, E, and F) to the end of the StringBuilder.
-
+        sb.Append(['D', 'E', 'J']);
 
         // TODO: Append a format string to the end of the StringBuilder.
+        sb.AppendFormat("GHI {0} {1}", 'J', 'k');
 
 
         // TODO: Display the number of characters in the StringBuilder and its string.
+        Console.WriteLine($"{sb.Length} chars, string: {sb.ToString()}");
 
 
         // TODO: Use AppendJoin to add an array of strings with a separator character
-
+        sb.AppendJoin("-", new char[] { 'L', 'M', 'N' });
 
         // TODO: Insert a string at the beginning of the StringBuilder.
+        sb.Insert(0, "Alphabet: ");
 
 
         // TODO: Replace all lowercase k's with uppercase K's.
-
+        sb.Replace('k', 'K');
 
         // TODO: Display the number of characters in the StringBuilder and its string.
+        Console.WriteLine($"Count: {sb.Length}, String: {sb.ToString()}");
+
 
     }
 }
+
