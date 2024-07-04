c4f4850517953bfa01c784e196249c13ec67002d
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-04 10:56:54 +0400

    Update Program.cs

--- Ch4_Specialized/OrderedDictExample/Program.cs
+++ Ch4_Specialized/OrderedDictExample/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Specialized;
 
@@ -6,24 +6,36 @@ namespace OrderedDictExample
 {
     class Program
     {
+
+        /// IMPORTANT
+        /// ORDERED DICTIONARIES will maintain 
+        /// in which items are added
+
         static void Main(string[] args)
         {
             // Create a new OrderedDictionary object
             OrderedDictionary mod = new OrderedDictionary();
 
             // TODO: Add some keys and values in a particular order
-
+            mod.Add("item1", "value1");
+            mod.Add("item2", "value2");
+            mod.Add("item3", "value3");
+            mod.Add("item4", "value4");
 
             // TODO: Print the contents of the Dictionary
-
+            PrintDictionary(mod);
 
             // TODO: Remove an existing item and add a new one
-
+            mod.Remove("item3");
+            mod.Add("item5", "value5");
 
             // TODO: Modify the value of an item in the dict
+            mod["item2"] = "new value 2";
 
 
             // TODO: Add one final new item
+            mod.Add("item6", "value 6");
+
 
 
         }
