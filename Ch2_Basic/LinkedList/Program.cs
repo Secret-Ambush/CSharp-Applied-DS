665f3516fb7d75306d1a6f3ddc1363371267f297
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-04 10:05:15 +0400

    Update Program.cs

--- Ch2_Basic/LinkedList/Program.cs
+++ Ch2_Basic/LinkedList/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinkedListExample
 {
@@ -13,25 +14,36 @@ namespace LinkedListExample
                             "All Night Long", "Right Here Right Now"};
 
             // TODO: Create a LinkedList that holds strings
-
+            LinkedList<string> mylist = new LinkedList<string>(songs);
 
             // TODO: Items can be added at the front or back of the list
-
+            mylist.AddFirst("Bones");
+            mylist.AddLast("Gods don't pray");
 
             // TODO: Like other collections, a LinkedList can be iterated
+            foreach (string s in mylist)
+            {
+                Console.WriteLine(s);
+            }
 
 
             // TODO: First and Last properties return LinkedListNodes
-
+            LinkedListNode<string> first = mylist.First;
+            LinkedListNode<string> last = mylist.Last;
 
             // TODO: Items can be added or removed relative to an existing item
-
+            mylist.AddAfter(first,"Blackbird");
+            foreach(string s in mylist)
+            {
+                Console.WriteLine(s);
+            }
 
             // TODO: Search for items in the list with Contains and Find
-
+            Console.WriteLine(mylist.Contains("Bones"));
+            Console.WriteLine(mylist.Find("Blackbird"));
 
             // TODO: The list can then be traversed with those properties
-
+            Console.WriteLine(first.Next.Value);
         }
     }
 }
