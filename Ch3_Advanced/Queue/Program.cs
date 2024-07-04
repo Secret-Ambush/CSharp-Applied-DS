d8179a38a55e82f6080a3c988a74d046ccfa2caf
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-04 10:13:28 +0400

    Update Program.cs

--- Ch3_Advanced/Queue/Program.cs
+++ Ch3_Advanced/Queue/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace QueueExample
@@ -8,28 +8,36 @@ namespace QueueExample
         static void Main(string[] args)
         {
             // Create an empty Queue that holds strings
-
+            Queue<string> myQ = new Queue<string>();
 
             // Add items to the end of the queue
-
+            myQ.Enqueue("one");
+            myQ.Enqueue("two");
+            myQ.Enqueue("three");
+            myQ.Enqueue("four");
+            myQ.Enqueue("five");
 
             // use the count property to see how many items there are
-
+            Console.WriteLine(myQ.Count);
 
             // Queues can be iterated without changing the structure
-
+            foreach (string s in myQ)
+            {
+                Console.WriteLine(s);
+            }
 
             // peek at the front of the queue
-
+            Console.WriteLine(myQ.Peek());
 
             // remove items from the front of the queue
-
+            myQ.Dequeue();
 
             // see if the queue contains an item
-
+            Console.WriteLine(myQ.Contains("one"));
 
             // Remove all the items
-
+            myQ.Clear();
+            Console.WriteLine(myQ.Count);
         }
     }
 }
